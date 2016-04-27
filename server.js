9d8779a1ad409e00e5c15d057cd3ab3dc63d57d3
Scott Diallo <diallot@me.com>
2016-04-27 08:24:12 -0700

    created add item

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
-
-var Storage = function() {
+var bodyParser = require('bodyParser');
+var jsonParser = bodyParser.json();
+var Storage = function() { // constructor
     this.items = [];
     this.id = 0;
 };
 
-Storage.prototype.add = function(name) {
+Storage.prototype.add = function(name) { // created a method .add
     var item = {name: name, id: this.id};
     this.items.push(item);
     this.id += 1;
@@ -23,5 +24,12 @@ app.use(express.static('public'));
 app.get('/items', function(req, res) {
     res.json(storage.items);
 });
+app.post('/items', jsonParser, function(req, res) {
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
 
+    var item = storage.add(req.body.name);
+    res.status(201).json(item);
+});
 app.listen(process.env.PORT || 8080);
